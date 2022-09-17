a574da62e9e58e5bb17d46862cd1e78aba236f0f
abrahao-gabriel <57734187+abrahao-gabriel@users.noreply.github.com>
2022-09-16 22:18:15 -0300

    3 files modified

--- src/app/components/gabriel-compound-interests/gabriel-compound-interests.component.ts
+++ src/app/components/gabriel-compound-interests/gabriel-compound-interests.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gabriel-compound-interests',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gabriel-compound-interests.component.css']
 })
 export class GabrielCompoundInterestsComponent implements OnInit {
-
+  @Input() c:string
+  @Input() i:string
+  @Input() t:string
   constructor() { }
 
   ngOnInit() {
   }
+  getJuros(){
+  let m = Number(this.c)*Math.pow(1+(Number(this.i)/100),Number(this.t))
 
+    return m-Number(this.c) 
+  }
 }
